dc15e1a654a80ea26eb6283ded609a211837aa22
wwyyzz <sdn_app@163.com>
2020-08-12 20:37:34 +0800

    init

---
+++ quickstart/src/test/java/com/wj/demo/QuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wj.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
